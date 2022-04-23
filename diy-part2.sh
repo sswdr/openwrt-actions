8b8b34e768e83f848efc99766ee33d3dd1fb6658
ssw <771020712@qq.com>
2022-04-24 01:13:34 +0800

    init

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,6 @@
 #
 
 # ---------------------------------------------------------------------------------------------------
-
 # 修改ip
 sed -i 's/192.168.1.1/192.168.2.254/g' package/base-files/files/bin/config_generate
 
@@ -22,75 +21,40 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci
 sed -i "s/OpenWrt /杀生丸大人 Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改主机名
-sed -i 's/OpenWrt/N1-ssw/g' package/base-files/files/bin/config_generate
-
-# samba4无效用户root：关闭
-sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
+sed -i 's/OpenWrt/SSWdeOpenWrt/g' package/base-files/files/bin/config_generate
 
 # Modify some code adaptation
 #sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
 
-# Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
-
-# 暂时未用：软件源修改
-# sed -i 's#openwrt.proxy.ustclug.org#mirrors.bfsu.edu.cn\\/openwrt#' package/lean/default-settings/files/zzz-default-settings
+# readd cpufreq for aarch64
+sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' feeds/luci/applications/luci-app-cpufreq/Makefile
+sed -i 's/services/system/g'  feeds/luci/applications/luci-app-cpufreq/luasrc/controller/cpufreq.lua
 
 # ---------------------------------------------------------------------------------------------------
-
 # （lean仓库没有的）拉取晶晨宝盒
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 # 晶晨宝盒相关配置
-sed -i 's#https://github.com/breakings/OpenWrt#自己的编译仓库地址例如https://github.com/quanjindeng/Actions_OpenWrt-Amlogic#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
+sed -i 's#https://github.com/breakings/OpenWrt#自己编译仓库地址例如https://github.com/breakings/OpenWrt#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 sed -i 's#ARMv8#openwrt_armvirt#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 sed -i 's#opt/kernel#kernel#g' package/luci-app-amlogic/luci-app-amlogic/root/etc/config/amlogic
 
-# 暂时未用：（lean仓库没有的）拉取openwrt-chinadns-ng
-git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
+# （lean仓库没有的）拉取关机luci插件
+git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
 
-# （lean仓库没有的）拉取网络存储 - luci-app-gowebdav
+# （lean仓库没有的）拉取网络存储 luci-app-gowebdav
 svn co https://github.com/immortalwrt-collections/openwrt-gowebdav/trunk/luci-app-gowebdav package/luci-app-gowebdav
 svn co https://github.com/immortalwrt-collections/openwrt-gowebdav/trunk/gowebdav package/gowebdav
 
-# 移除lean仓库luci的argon主题
-rm -rf feeds/luci/themes/luci-theme-argon
+# （lean仓库没有的）拉取扩展插件库 kenzok8：openwrt-packages
+git clone https://github.com/kenzok8/small package/small
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+# 删除重复插件
+rm -rf package/openwrt-packages/luci-app-amlogic
+rm -rf package/openwrt-packages/UnblockNeteaseMusic
+rm -rf package/openwrt-packages/luci-app-unblockneteasemusic
+rm -rf package/openwrt-packages/luci-theme-argon
+rm -rf package/openwrt-packages/luci-app-argon-config
 
-# 拉取jerrykuku仓库的argon主题：18.06
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-
-# 拉取解锁音乐
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-# 解锁音乐修改
-NAME=$"package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
-curl 'https://api.github.com/repos/UnblockNeteaseMusic/server/commits?sha=enhanced&path=precompiled' -o commits.json
-echo "$(grep sha commits.json | sed -n "1,1p" | cut -c 13-52)">"$NAME/core_local_ver"
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/bridge.js -o $NAME/core/bridge.js
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/ca.crt -o $NAME/core/ca.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.crt -o $NAME/core/server.crt
-curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/server.key -o $NAME/core/server.key
-
-# 拉取关机luci插件
-# git clone https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice
-# git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-
-# passwall插件
-# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
-
-# helloworld插件
-# git clone https://github.com/fw876/helloworld package/helloworld
-
-# 拉取kenzok8的small-package软件包（同名软件包默认使用lean的，要使用其他仓库插件，先删除lean的再clone其他库）
-git clone https://github.com/kenzok8/small-package package/small-package
-# 删除重复包
-rm -rf package/small-package/luci-app-openvpn-server
-rm -rf package/small-package/openvpn-easy-rsa-whisky
-rm -rf package/small-package/luci-app-wrtbwmon
-rm -rf package/small-package/wrtbwmon
-rm -rf package/small-package/luci-app-koolproxyR
-rm -rf package/small-package/luci-app-godproxy
-rm -rf package/small-package/luci-app-argon*
-rm -rf package/small-package/luci-theme-argon*
-rm -rf package/small-package/luci-app-amlogic
-rm -rf package/small-package/luci-app-unblockneteasemusic
+./scripts/feeds update -a
+./scripts/feeds install -a
