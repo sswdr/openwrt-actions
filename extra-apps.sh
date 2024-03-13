9d668504c660c04851b3540971e91ce338d0a1d5
sswdr <771020712@qq.com>
2024-03-13 11:53:30 +0800

    2024.new

--- extra-apps.sh
+++ extra-apps.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 #
 # 只增删插件仓库/修改feeds
+# 指定分支 [extra-apps.sh {main/backup.2024.01.01} {main}]指定kenzok8/openwrt-packages-ssw分支
 #
 
-# 自用备份插件库 by ssw, main分支
-git clone https://github.com/rin0612/openwrt-packages-ssw  package/openwrt-packages-ssw
+BRANCH_SSW=${1:-main}
 
-# kenzok8 packages by backup.2022.04.25
-git clone https://github.com/rin0612/openwrt-packages  package/openwrt-packages -b backup.2022.04.25
+BRANCH=${1:-backup.2022.04.25}
 
-# kenzok8 vpn相关 by backup.2022.04.25
-git clone https://github.com/rin0612/small  package/small -b backup.2022.04.25
+# 自用备份插件库
+git clone https://github.com/rin0612/openwrt-packages-ssw  package/openwrt-packages-ssw -b $BRANCH_SSW
+
+# kenzok8 packages
+git clone https://github.com/rin0612/openwrt-packages  package/openwrt-packages -b $BRANCH
+
+# kenzok8 vpn相关
+git clone https://github.com/rin0612/small  package/small -b $BRANCH
 
 rm -rf package/openwrt-packages/UnblockNeteaseMusic
 rm -rf package/openwrt-packages/luci-app-unblockneteasemusic
